49b1945936298a8560a96006067135d501cc837b
R.Lee <rleearts@gmail.com>
2026-04-15 09:32:29 -0400

    Practice UserData. Implement CoreConcepts()

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import reactImg from './assets/react-core-concepts.png';
+//import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
+/*
+function UserData(){
+  const user = {first:"George", last:"Salayka", age:"40"}
+  return (
+    <div>
+      <h2>Name: {user.last}, {user.first}</h2>
+      <h3>Age: {user.age}</h3>
+    </div>
+  )
+}
+*/
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.image} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
 
 function Header() {
   const reactDescriptions = ('Fundamental', 'Crucial', 'Core');
-  function genRandomInt(max){
-    return Math.floor(Math.random() * (max+1));
+  function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
   }
-  const description = reactDescriptions [genRandomInt(2)];
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
@@ -26,10 +49,44 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title = {CORE_CONCEPTS[0].title}
+              description = {CORE_CONCEPTS[0].description}
+              image ={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title = {CORE_CONCEPTS[1].title}
+              description = {CORE_CONCEPTS[1].description}
+              image ={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title = {CORE_CONCEPTS[2].title}
+              description = {CORE_CONCEPTS[2].description}
+              image ={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title = {CORE_CONCEPTS[3].title}
+              description = {CORE_CONCEPTS[3].description}
+              image ={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
 
 export default App;
+
+/*
+Notes:
+!! Shortcut: Shift + Option + F
+<UserData /> was removed from App() after </main>. This is required to run the UserData function
+Create CoreConcepts(), Then created specialized ver. passing {props.???}, Make sure you import assest
+Downloaded data.js and placed into src folder.
+!! Shortcut: Cmd + D while hove over a repeated element selects every dupe after per shorcut. Fast replace!
+
+*/
